6ed9672870163946999f0bc11a51f15b28949652
Biniam Gheb <luwyzb@gmail.com>
2024-09-23 23:30:02 -0700

    project-created

---
+++ src/main/java/com/biniam/songyzmart/SongyzMartApplication.java
@@ -0,0 +1,13 @@
+package com.biniam.songyzmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongyzMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongyzMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biniam/songyzmart/SongyzMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biniam.songyzmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongyzMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
